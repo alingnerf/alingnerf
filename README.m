e9912588950d55b459ad075be4227f27a5da54ee
alingnerf <silongannorhan9@gmail.com>
2024-10-11 21:29:22 +0800

    Yellow

---
+++ README.m
@@ -0,0 +1,11 @@
+echo "# alingnerf" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alingnerf/alingnerf.git
+git push -u origin main
+<!---
+alingnerf/alingnerf is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
